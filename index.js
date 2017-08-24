8d59c8636a2941e517ed89a92abc79f558e7aab4
Ubuntu <ubuntu@ip-172-31-47-122.us-west-2.compute.internal>
2017-08-24 20:36:55 +0000

    asd

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-
-var bodyParser = require('body-parser')
-app.use(bodyParser());
+const whitelist = require('./whitelist.js')
+const cors = require('cors')
 
 
 app.get('/', (req, res) => {
