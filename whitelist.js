6e7d76df3c3c614fcddf0faaefb87acd6acecf82
Bryce Roche <bryceroche@bryces-imac.lan>
2017-08-30 16:57:59 -0700

    d

--- whitelist.js
+++ whitelist.js
@@ -3,6 +3,7 @@ exports.Whitelist = function (){
 		'www.recruitercall-trugreen.com' : true,
 		'www.recruiting.adri-sys.com': true,
 		'https://s3.amazonaws.com/aws-website-adridemo-aj5ft': true,
-		'http://ec2-54-149-95-169.us-west-2.compute.amazonaws.com/*': true
+		'http://ec2-54-149-95-169.us-west-2.compute.amazonaws.com/*': true,
+		'' : true
 	};
 };
