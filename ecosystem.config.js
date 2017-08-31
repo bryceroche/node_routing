761b7eecbcf8cf4fb0b288e0889df256a6c92c7c
Bryce Roche <bryceroche@bryces-imac.lan>
2017-08-31 07:45:33 -0700

    config file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'node_routing',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-149-95-169.us-west-2.compute.amazonaws.com',
+      key: '~/Desktop/node_routing/ec2_expressv1.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/bryceroche/node_routing',
+      path: '/home/ubuntu/server/hello-nodejs',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
